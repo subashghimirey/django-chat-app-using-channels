0d8649aa7bde535fae03b6bffa8feb3fc4d07da2
Subash <ghimiresubash980843@gmail.com>
2024-05-12 00:29:52 +0545

    version-2

--- chat_room/consumers.py
+++ chat_room/consumers.py
@@ -14,6 +14,7 @@ class ChatConsumer(WebsocketConsumer):
         self.room_group_name = None
         self.room = None
         self.user = None
+        self.user_inbox = None
 
     
     def connect(self):
@@ -21,7 +22,7 @@ class ChatConsumer(WebsocketConsumer):
         self.room_group_name = f'chat_{self.room_name}'
         self.room = Room.objects.get(name=self.room_name)
         self.user = self.scope['user']
-
+        self.user_inbox = f'inbox_{self.user.username}'
         self.accept()
 
         # join the room group
@@ -30,11 +31,54 @@ class ChatConsumer(WebsocketConsumer):
             self.channel_name
         )
 
+        # send the user list to the newly joined user
+        self.send(json.dumps({
+            'type': 'user_list',
+            'users': [user.username for user in self.room.online.all()],
+        }))
+
+        if self.user.is_authenticated:
+            
+            # create a user inbox for private messages
+            async_to_sync(self.channel_layer.group_add)(
+                self.user_inbox,
+                self.channel_name,
+            )
+
+            # send the join event to the room
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'user_join',
+                    'user': self.user.username,
+                }
+            )
+            self.room.online.add(self.user)
+
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
+
+        if self.user.is_authenticated:
+
+            # delete the user inbox for private messages
+            async_to_sync(self.channel_layer.group_discard)(
+                self.user_inbox,
+                self.channel_name,
+            )
+
+
+            # send the leave event to the room
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {
+                    'type': 'user_leave',
+                    'user': self.user.username,
+                }
+            )
+            self.room.online.remove(self.user)
 
     
     def receive(self, text_data=None, bytes_data=None):
@@ -42,6 +86,28 @@ class ChatConsumer(WebsocketConsumer):
         message = text_data_json['message']
 
         if not self.user.is_authenticated:
+            return
+        
+        if message.startswith('/pm '):
+            split = message.split(' ', 2)
+            target = split[1]
+            target_msg = split[2]
+
+            # send private message to the target
+            async_to_sync(self.channel_layer.group_send)(
+                f'inbox_{target}',
+                {
+                    'type': 'private_message',
+                    'user': self.user.username,
+                    'message': target_msg,
+                }
+            )
+            # send private message delivered to the user
+            self.send(json.dumps({
+                'type': 'private_message_delivered',
+                'target': target,
+                'message': target_msg,
+            }))
             return
 
         async_to_sync(self.channel_layer.group_send)(
@@ -57,3 +123,17 @@ class ChatConsumer(WebsocketConsumer):
 
     def chat_message(self, event):
         self.send(text_data=json.dumps(event))
+
+    
+    def user_join(self, event):
+        self.send(text_data=json.dumps(event))
+
+    def user_leave(self, event):
+        self.send(text_data=json.dumps(event))
+
+    def private_message(self, event):
+        self.send(text_data=json.dumps(event))
+
+    def private_message_delivered(self, event):
+        self.send(text_data=json.dumps(event))
+        
--- chat_room/static/room.js
+++ chat_room/static/room.js
@@ -16,6 +16,12 @@ function onlineUsersSelectorAdd(value) {
     onlineUsersSelector.appendChild(newOption);
 }
 
+onlineUsersSelector.onchange = function() {
+    chatMessageInput.value = "/pm " + onlineUsersSelector.value + " ";
+    onlineUsersSelector.value = null;
+    chatMessageInput.focus();
+};
+
 // removes an option from 'onlineUsersSelector'
 function onlineUsersSelectorRemove(value) {
     let oldOption = document.querySelector("option[value='" + value + "']");
@@ -66,6 +72,26 @@ function connect() {
         switch (data.type) {
             case "chat_message":
                 chatLog.value += data.user + ": " + data.message + "\n"; 
+                break;
+             case "user_list":
+                for (let i = 0; i < data.users.length; i++) {
+                    onlineUsersSelectorAdd(data.users[i]);
+                }
+                break;
+            case "user_join":
+                chatLog.value += data.user + " joined the room.\n";
+                onlineUsersSelectorAdd(data.user);
+                break;
+            case "user_leave":
+                chatLog.value += data.user + " left the room.\n";
+                onlineUsersSelectorRemove(data.user);
+                break;      
+                
+            case "private_message":
+                chatLog.value += "PM from " + data.user + ": " + data.message + "\n";
+                break;
+            case "private_message_delivered":
+                chatLog.value += "PM to " + data.target + ": " + data.message + "\n";
                 break;
             default:
                 console.error("Unknown message type!");
